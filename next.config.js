e13f5508d8117d0562012578a99481ac99bec308
George Kasmiro Quiriko <georgekasdc@icloud.com>
2026-01-06 21:09:02 +0530

    add favicon

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // PWA support
-  experimental: {
-    appDir: true,
-  },
 };
 
 module.exports = nextConfig;
